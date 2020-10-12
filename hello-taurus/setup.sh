5b32e6247ff812406a0d23de794a134070f81cdd
Xavi Ametller <xavi.ametller@gmail.com>
2020-10-12 22:45:35 +0200

    Add checkout of demo website

--- hello-taurus/setup.sh
+++ hello-taurus/setup.sh
@@ -1,2 +1,9 @@
 echo "hello world Taurus!"
 docker pull blazemeter/taurus
+
+cd /usr/local/
+git clone https://github.com/jamstack-cms/jamstack-ecommerce
+cd jamstack-ecommerce
+yarn
+npm install -g gatsby-cli
+gatsby develop

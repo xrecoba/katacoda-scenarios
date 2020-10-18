c4ea74f6a203598a03f43564a97d2ffd16e30e85
Xavi Ametller <xavi.ametller@gmail.com>
2020-10-19 00:07:50 +0200

    Moved load test file and web repo into the same folder

--- hello-taurus/setup.sh
+++ hello-taurus/setup.sh
@@ -1,7 +1,7 @@
 echo "hello world Taurus!"
 docker pull blazemeter/taurus
 
-cd /usr/local/
+cd /usr/local/web-test-automation
 git clone https://github.com/xrecoba/web-testing-playground.git
 cd web-testing-playground
 npm install
